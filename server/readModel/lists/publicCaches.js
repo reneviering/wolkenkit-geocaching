23453b131fd72b05406a7716cb88e38f93833f2e
René Viering <rene.viering@gmail.com>
2017-12-02 20:56:04 +0100

    Apply eslint-plugin-wolkenit rules to the code style.

--- server/readModel/lists/publicCaches.js
+++ server/readModel/lists/publicCaches.js
@@ -1,55 +1,55 @@
 'use strict';
 
 const fields = {
-	name: { initialState: '' },
-	description: {initialState: ''},
-	coordinate: {initialState: {lat: 0, lon: 0}},
-	countFavorites: {initialState: 0},
-  countFindings: {initialState: 0},
-	comments: {initialState: []}
+  name: { initialState: '' },
+  description: { initialState: '' },
+  coordinate: { initialState: { lat: 0, lon: 0 }},
+  countFavorites: { initialState: 0 },
+  countFindings: { initialState: 0 },
+  comments: { initialState: []}
 };
 
 const when = {
-	'geocaching.cache.published': (caches, event, mark) => {
-		caches.add({
-			name: event.data.name,
-			description: event.data.description,
-			coordinate: event.data.coordinate,
-		});
+  'geocaching.cache.published': (publicCaches, event, mark) => {
+    publicCaches.add({
+      name: event.data.name,
+      description: event.data.description,
+      coordinate: event.data.coordinate
+    });
 
-		mark.asDone();
-	},
+    mark.asDone();
+  },
 
-	'geocaching.cache.favored': (caches, event, mark) => {
-		caches.update({
-			where: {id: event.aggregate.id},
-			set: {
-				countFavorites: event.data.countFavorites
-			}
-		});
-		mark.asDone();
-	},
+  'geocaching.cache.favored': (publicCaches, event, mark) => {
+    publicCaches.update({
+      where: { id: event.aggregate.id },
+      set: {
+        countFavorites: event.data.countFavorites
+      }
+    });
+    mark.asDone();
+  },
 
-	'geocaching.cache.found': (caches, event, mark) => {
-		caches.update({
-			where: {id: event.aggregate.id},
-			set: {
-				countFindings: event.data.countFindings,
-				comments: event.data.comments
-			}
-		});
-		mark.asDone();
-	},
+  'geocaching.cache.found': (publicCaches, event, mark) => {
+    publicCaches.update({
+      where: { id: event.aggregate.id },
+      set: {
+        countFindings: event.data.countFindings,
+        comments: event.data.comments
+      }
+    });
+    mark.asDone();
+  },
 
-	'geocaching.cache.commented': (caches, event, mark) => {
-		caches.update({
-			where: {id: event.aggregate.id},
-			set: {
-				comments: event.data.comments
-			}
-		});
-		mark.asDone();
-	}
+  'geocaching.cache.commented': (publicCaches, event, mark) => {
+    publicCaches.update({
+      where: { id: event.aggregate.id },
+      set: {
+        comments: event.data.comments
+      }
+    });
+    mark.asDone();
+  }
 };
 
 module.exports = { fields, when };
--- server/writeModel/geocaching/cache.js
+++ server/writeModel/geocaching/cache.js
@@ -1,145 +1,145 @@
 'use strict';
 
 const initialState = {
-	name: '',
-	description: '',
-	coordinate: {lat: 0, lon: 0},
-	countFavorites: 0,
-	published: false,
-	countFindings: 0,
-	comments: [],
-	removed: false,
+  name: '',
+  description: '',
+  coordinate: { lat: 0, lon: 0 },
+  countFavorites: 0,
+  published: false,
+  countFindings: 0,
+  comments: [],
+  removed: false,
 
-	isAuthorized: {
-		commands: {
-			publish: { forAuthenticated: true, forPublic: false },
-			favor: { forAuthenticated: true, forPublic: false },
-			find: { forAuthenticated: true, forPublic: false },
-			comment: { forAuthenticated: true, forPublic: false }
-		},
-		events: {
-			published: { forAuthenticated: true, forPublic: false },
-			favored: { forAuthenticated: true, forPublic: false },
-			found: { forAuthenticated: true, forPublic: false },
-			commented: { forAuthenticated: true, forPublic: false }
-		}
-	}
+  isAuthorized: {
+    commands: {
+      publish: { forAuthenticated: true, forPublic: false },
+      favor: { forAuthenticated: true, forPublic: false },
+      find: { forAuthenticated: true, forPublic: false },
+      comment: { forAuthenticated: true, forPublic: false }
+    },
+    events: {
+      published: { forAuthenticated: true, forPublic: false },
+      favored: { forAuthenticated: true, forPublic: false },
+      found: { forAuthenticated: true, forPublic: false },
+      commented: { forAuthenticated: true, forPublic: false }
+    }
+  }
 };
 
 const commands = {
-	hide(cache, command, mark) {
-		cache.events.publish('hidden', {
-			name: command.data.name,
-			description: command.data.description,
-			coordinate: {
-				lat: command.data.coordinate.lat,
-				lon: command.data.coordinate.lon
-			}
-		});
-		mark.asDone();
-	},
+  hide (cache, command, mark) {
+    cache.events.publish('hidden', {
+      name: command.data.name,
+      description: command.data.description,
+      coordinate: {
+        lat: command.data.coordinate.lat,
+        lon: command.data.coordinate.lon
+      }
+    });
+    mark.asDone();
+  },
 
-	publish(cache, command, mark) {
-		cache.events.publish('published', {
-			name: cache.state.name,
-			description: cache.state.description,
-			coordinate: {
-				lat: cache.state.coordinate.lat,
-				lon: cache.state.coordinate.lon
-			},
+  publish (cache, command, mark) {
+    cache.events.publish('published', {
+      name: cache.state.name,
+      description: cache.state.description,
+      coordinate: {
+        lat: cache.state.coordinate.lat,
+        lon: cache.state.coordinate.lon
+      },
       countFavorites: cache.state.countFavorites,
-    	countFindings: cache.state.countFindings,
-    	comments: cache.state.comments,
-    	removed: cache.state.removed,
-			published: true,
-		});
-		mark.asDone();
-	},
+      countFindings: cache.state.countFindings,
+      comments: cache.state.comments,
+      removed: cache.state.removed,
+      published: true
+    });
+    mark.asDone();
+  },
 
-	favor(cache, command, mark) {
-		cache.events.publish('favored', {
-			countFavorites: cache.state.countFavorites + 1
-		});
-		mark.asDone();
-	},
+  favor (cache, command, mark) {
+    cache.events.publish('favored', {
+      countFavorites: cache.state.countFavorites + 1
+    });
+    mark.asDone();
+  },
 
-	find(cache, command, mark) {
-		cache.events.publish('found', {
-			comments: [...cache.state.comments, {
-				text: command.data.text,
-				finder: command.user.id,
-				timestamp: command.metadata.timestamp
-			}],
-			countFindings: cache.state.countFindings + 1
-		});
-		mark.asDone();
-	},
+  find (cache, command, mark) {
+    cache.events.publish('found', {
+      comments: [ ...cache.state.comments, {
+        text: command.data.text,
+        finder: command.user.id,
+        timestamp: command.metadata.timestamp
+      }],
+      countFindings: cache.state.countFindings + 1
+    });
+    mark.asDone();
+  },
 
-	comment(cache, command, mark) {
-		cache.events.publish('commented', {
-			comments: [...cache.state.comments, {
-				text: command.data.text,
-				author: command.user,
-				timestamp: command.metadata.timestamp
-			}],
-		});
-		mark.asDone();
-	},
+  comment (cache, command, mark) {
+    cache.events.publish('commented', {
+      comments: [ ...cache.state.comments, {
+        text: command.data.text,
+        author: command.user,
+        timestamp: command.metadata.timestamp
+      }]
+    });
+    mark.asDone();
+  },
 
-	remove(cache, command, mark) {
-		if(cache.state.published) {
-			 return mark.asRejected('An already published cache can not be removed');
-		}
+  remove (cache, command, mark) {
+    if (cache.state.published) {
+      return mark.asRejected('An already published cache can not be removed');
+    }
 
-		cache.events.publish('removed');
-		mark.asDone();
-	}
+    cache.events.publish('removed');
+    mark.asDone();
+  }
 };
 
 const events = {
-	hidden(cache, event) {
-		cache.setState({
-			name: event.data.name,
-			description: event.data.description,
-			coordinate: event.data.coordinate
-		})
-	},
+  hidden (cache, event) {
+    cache.setState({
+      name: event.data.name,
+      description: event.data.description,
+      coordinate: event.data.coordinate
+    });
+  },
 
-	favored(cache, event) {
-		cache.setState({
-			countFavorites: event.data.countFavorites
-		})
-	},
+  favored (cache, event) {
+    cache.setState({
+      countFavorites: event.data.countFavorites
+    });
+  },
 
-	published(cache, event) {
-		cache.setState({
-			published: event.data.published
-		})
-	},
+  published (cache, event) {
+    cache.setState({
+      published: event.data.published
+    });
+  },
 
-	found(cache, event) {
-		cache.setState({
-			countFindings: event.data.countFindings,
-			comments: event.data.comments,
-			finder: event.data.finder
-		});
-	},
+  found (cache, event) {
+    cache.setState({
+      countFindings: event.data.countFindings,
+      comments: event.data.comments,
+      finder: event.data.finder
+    });
+  },
 
-	commented(cache, event) {
-		cache.setState({
-			comments: event.data.comments
-		});
-	},
+  commented (cache, event) {
+    cache.setState({
+      comments: event.data.comments
+    });
+  },
 
-	removed(cache, event) {
-		cache.setState({
-			removed: true
-		});
-	}
+  removed (cache) {
+    cache.setState({
+      removed: true
+    });
+  }
 };
 
 module.exports = {
-	initialState,
-	commands,
-	events
+  initialState,
+  commands,
+  events
 };
